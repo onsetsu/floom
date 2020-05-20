b917fe38baa521c53e1d056c5b904303e998640e
birneamstiel <luckifri@yahoo.de>
2020-05-21 01:57:11 +0200

    Add snapshotting logic for obstacle.

--- src/floom/obstacle.js
+++ src/floom/obstacle.js
@@ -4,5 +4,19 @@ import Vector2 from "./../external/vector2.js";
 		this.position = new Vector2(x, y);
 		this.radius = radius;
 	};
+
+	Obstacle.prototype.toJSON = function() {
+		let settings = {
+			position: this.position,
+			radius: this.radius
+		};
+
+		return JSON.stringify(settings);
+	};
+
+	Obstacle.fromJSON = function(settings) {
+		let parsedSettings = JSON.parse(settings);
+		return new Obstacle(parsedSettings.position.x, parsedSettings.position.y, parsedSettings.radius)
+	};
 	
 	export default Obstacle;
