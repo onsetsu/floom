69f7ac8de5bb25b0bd1b24d0c2b160ba140d8005
jonasnoki <jonasnoackgruen@gmail.com>
2020-06-19 14:25:35 +0200

    Replace math.matrix with glMatrix.mat2

--- src/floom/elasticitySimulation.js
+++ src/floom/elasticitySimulation.js
@@ -1,11 +1,14 @@
 import System from "./system.js";
 import Vector2 from "../external/vector2.js";
+// import * as glMatrix from "../external/glMatrix.js";
 
 // Lamé parameters for stress-strain relationship
 const elastic_lambda = 10.0;
 const elastic_mu = 20.0;
 // TODO: calculate this dynamically, depending on FPS. CAUTION: when this value is too low, NaNs will occur.
-const timeStep = 0.2;
+const timeStep = 0.02;
+const mat2 = glMatrix.mat2;
+const vec2 = glMatrix.vec2;
 
 /*
  * Simple elasticity implementation of MPM taken from https://github.com/nialltl/incremental_mpm
@@ -44,48 +47,58 @@ System.prototype.__elasticParticleToGrid = function() {
 
 	this.particles.forEach(function(p, particleIndex) {
 
-		const J = math.det(p.deformationGradient);
+		const J = mat2.determinant(p.deformationGradient);
 		// TODO: what's the volume about? There also seems to be am
 		const volume = p.initialVolume * J;
-		const F_T = math.transpose(p.deformationGradient);
-		const F_inv_T = math.inv(F_T);
-		const F_minus_F_inv_T = math.subtract(p.deformationGradient, F_inv_T);
+		const F_T = mat2.transpose(mat2.create(), p.deformationGradient);
+		const F_inv_T = mat2.invert(mat2.create(), F_T);
+		const F_minus_F_inv_T = mat2.subtract(mat2.create(), p.deformationGradient, F_inv_T);
 
 		// MPM course equation 48
-		const P_term_0 = math.multiply(F_minus_F_inv_T ,elastic_mu);
-		const P_term_1 = math.multiply(F_inv_T, elastic_lambda * math.log(J));
-		const P = math.add(P_term_0, P_term_1);
+		const P_term_0 = mat2.multiplyScalar(mat2.create(), F_minus_F_inv_T ,elastic_mu);
+		const P_term_1 = mat2.multiplyScalar(mat2.create(), F_inv_T, elastic_lambda * Math.log(J));
+		const P = mat2.add(mat2.create(), P_term_0, P_term_1);
 
 
 
 		// cauchy_stress = (1 / det(F)) * P * F_T
 		// equation 38, MPM course
-		const stress = math.multiply((1.0 / J), math.multiply(P, F_T));
-
+		const stress = mat2.multiplyScalar(
+			mat2.create(),
+			mat2.multiply(mat2.create(), P, F_T),
+			(1.0 / J));
 
 
 		// (M_p)^-1 = 4, see APIC paper and MPM course page 42
 		// this term is used in MLS-MPM paper eq. 16. with quadratic weights, Mp = (1/4) * (delta_x)^2.
 		// in this simulation, delta_x = 1, because i scale the rendering of the domain rather than the domain itself.
 		// we multiply by dt as part of the process of fusing the momentum and force update for MLS-MPM
-		const eq_16_term_0 = math.multiply(stress, -volume * 4 * timeStep);
+		const eq_16_term_0 = mat2.multiplyScalar(mat2.create(), stress, -volume * 4 * timeStep);
 
 		this.integrator.updateStateAndGradientOf(p);
 		this.integrator.prepareParticle(p);
 
 		this.integrator.integrate(p, function(particle, node, phi, gxpy, pxgy){
-			const cell_dist = [gxpy, pxgy];
-			const Q = Vector2.fromMathMatrix(math.multiply(p.affineMomentum, cell_dist));
+			const cell_dist = vec2.create(gxpy, pxgy);
+			const Q = Vector2.fromGlMatrixVec2(
+				vec2.transformMat2(
+					vec2.create(),
+					cell_dist,
+					p.affineMomentum));
+
+			// const Q = Vector2.fromMathMatrix(math.multiply(p.affineMomentum, cell_dist));
 
 			const weightedMass = phi * particle.material.particleMass;
 			node.mass += weightedMass;
 			node.velocity.addSelf((particle.velocity.add(Q).mulFloat(weightedMass)));
 
 
-
 			// fused force/momentum update from MLS-MPM
 			// see MLS-MPM paper, equation listed after eqn. 28
-			const momentum = Vector2.fromMathMatrix(math.multiply(math.multiply(eq_16_term_0, phi), cell_dist));
+			const momentum = Vector2.fromGlMatrixVec2(vec2.transformMat2(
+				vec2.create(),
+				cell_dist,
+				mat2.multiplyScalar(mat2.create(), eq_16_term_0, phi)));
 			node.velocity.addSelf(momentum);
 
 
@@ -124,18 +137,18 @@ System.prototype.__elasticGridToParticle = function() {
 		// see APIC paper (https://web.archive.org/web/20190427165435/https://www.math.ucla.edu/~jteran/papers/JSSTS15.pdf), page 6
 		// below equation 11 for clarification. this is calculating C = B * (D^-1) for APIC equation 8,
 		// where B is calculated in the inner loop at (D^-1) = 4 is a constant when using quadratic interpolation functions
-		let B = math.zeros(2, 2);
+		let B = mat2.fromValues(0,0,0,0);;
 
 		this.integrator.integrate(p, function(particle, node, phi, gxpy, pxgy){
 
 			const weighted_velocity = new Vector2(node.velocity.x * phi, node.velocity.y * phi);
 
 			// APIC paper equation 10, constructing inner term for B
-			const term = math.matrix([
-				[weighted_velocity.x * gxpy, weighted_velocity.y * gxpy],
-				[weighted_velocity.x * pxgy, weighted_velocity.y * pxgy]]);
+			const term = mat2.fromValues(
+				weighted_velocity.x * gxpy, weighted_velocity.x * pxgy,
+				weighted_velocity.y * gxpy, weighted_velocity.y * pxgy);
 
-			B = math.add(B, term);
+			B = mat2.add(B, B, term);
 			particle.velocity.addSelf(weighted_velocity);
 
 			if(isNaN(particle.velocity.x) || isNaN(particle.velocity.y)) {
@@ -152,15 +165,23 @@ System.prototype.__elasticGridToParticle = function() {
 			this.alreadyBreaked = true;
 		}
 
-		p.affineMomentum = math.multiply(B, 4);
+		p.affineMomentum = mat2.multiplyScalar(p.affineMomentum, B, 4);
 		p.position.addSelf(p.velocity.mulFloat(timeStep));
 
-		let newDeformationGradient = math.matrix([
-			[1, 0],
-			[0, 1]]);
+		let newDeformationGradient = mat2.create();
 
-		newDeformationGradient = math.add(newDeformationGradient, math.multiply(p.affineMomentum, timeStep));
-		p.deformationGradient = math.multiply(newDeformationGradient, p.deformationGradient);
+		newDeformationGradient = mat2.add(
+			newDeformationGradient,
+			newDeformationGradient,
+			mat2.multiplyScalar(
+				mat2.create(),
+				p.affineMomentum,
+				timeStep));
+
+		p.deformationGradient = mat2.multiply(
+			p.deformationGradient,
+			newDeformationGradient,
+			p.deformationGradient);
 	}, this);
 };
 
